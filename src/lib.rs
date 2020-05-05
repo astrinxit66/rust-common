b91ecdb169e3d59bff63adaad23499831f18ce57
astrinxit66 <rjeanmarc@me.com>
2020-05-05 17:13:02 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub mod logger;
+pub mod cfg;
+pub mod codec;
+
+pub use log;
+
+pub mod path {
+    use std::{
+        path::PathBuf,
+        env
+    };
+
+    pub fn from_app_root(rel_path: &str) -> PathBuf {
+        let mut cwd = env::current_dir().expect("get current working directory");
+
+        cwd.push(rel_path);
+        cwd
+    }
+}
